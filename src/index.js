4a66efea191ed59532e0d40e827d3e2a7ce03d33
khaliasjohnson25 <115362382+khaliasjohnson25@users.noreply.github.com>
2023-03-29 15:32:51 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,3 +11,6 @@ root.render(
     <App />
   </StrictMode>
 );
+
+  Openly sourced by: Khalia Santos-Johnson
+ <a href= 'https://codesandbox.io/p/github/khaliasjohnson25/howyouReactEngine/main?create=true&file=%2FREADME.md'></a>
